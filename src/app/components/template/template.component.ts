16ed569cce76268323d103b4f208df48098f5cda
Nicolas Bloj <nicolasbloj@gmail.com>
2017-10-26 18:50:49 -0300

    template method - ngForm

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,3 +1,4 @@
+import { NgForm } from '@angular/forms/src/directives';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,9 +8,18 @@ import { Component } from '@angular/core';
 })
 export class TemplateComponent {
 
+  user: Object = {
+    name: 'nico',
+    lastname: 'bloj',
+    email: 'nico@gmail.com',
+  };
+
   constructor() { }
 
-  save() {
+  save(form: NgForm) {
     console.log('formulario posteado');
+    console.log('ngForm', form);
+    console.log('ngForm.value', form.value);
+    console.log('usuario', this.user);
   }
 }

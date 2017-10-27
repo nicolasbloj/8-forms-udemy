ba17bb4d45d1d75a55690f673910cbdcf8dda20b
Nicolas Bloj <nicolasbloj@gmail.com>
2017-10-27 17:28:05 -0300

    validators minLength

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -12,7 +12,7 @@ export class DataComponent {
 
   constructor() {
     this.form = new FormGroup({
-      'nombre': new FormControl('', Validators.required),
+      'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
       'apellido': new FormControl('', Validators.required),
       'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
     });
